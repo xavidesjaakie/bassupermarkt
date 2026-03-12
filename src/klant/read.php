a61b17bf708ee4aa1cf1fa59271f09c1c6426e45
xavidesjaakie <kakerkak11@gmail.com>
2026-03-12 08:54:06 +0100

    Refactor klanten overview page with HTML and CSS
    Updated the HTML structure and styling for the klanten overview page.

--- src/klant/read.php
+++ src/klant/read.php
@@ -9,27 +9,46 @@ $klant = new Klant($db);
 $klanten = $klant->readAll();
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Klanten Overzicht</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
 <h2>Klanten Overzicht</h2>
 
-<a href="insert.php">Nieuwe klant</a>
+<a class="btn" href="insert.php">Nieuwe klant</a>
 
-<table border="1">
+<table>
+
 <tr>
-    <th>ID</th>
-    <th>Naam</th>
-    <th>Email</th>
-    <th>Acties</th>
+<th>ID</th>
+<th>Naam</th>
+<th>Email</th>
+<th>Acties</th>
 </tr>
 
 <?php foreach ($klanten as $row): ?>
+
 <tr>
-    <td><?= $row['klantId']; ?></td>
-    <td><?= $row['klantNaam']; ?></td>
-    <td><?= $row['klantEmail']; ?></td>
-    <td>
-        <a href="update.php?id=<?= $row['klantId']; ?>">Wijzig</a>
-        <a href="delete.php?id=<?= $row['klantId']; ?>">Verwijder</a>
-    </td>
+<td><?= $row['klantId']; ?></td>
+<td><?= $row['klantNaam']; ?></td>
+<td><?= $row['klantEmail']; ?></td>
+
+<td>
+<a class="edit" href="update.php?id=<?= $row['klantId']; ?>">Wijzig</a>
+<a class="delete" href="delete.php?id=<?= $row['klantId']; ?>">Verwijder</a>
+</td>
+
 </tr>
+
 <?php endforeach; ?>
+
 </table>
+
+</body>
+</html>
